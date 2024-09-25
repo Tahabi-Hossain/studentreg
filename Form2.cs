2b90f0018f7576f396294bf971324a001e53d5a8
Tahabi <tahabihossain714@gmail.com>
2024-09-25 11:38:54 +0600

    first commit

---
+++ Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Acer\\Documents\\demo.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void Reset()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+        }
+
+        private void populate()
+        {
+            con.Open();
+            string query = "select * from InfoTable";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            con.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Missing Information");
+            }
+
+            else
+            {
+                try
+                {
+                    string query = "insert into InfoTable values('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "')";
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Information Successfully Saved");
+                    con.Close();
+                    populate();
+                    Reset();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update InfoTable set Id=@Id, FirstName=@fName, LastName=@lname, Age=@Age", con);
+            cmd.Parameters.AddWithValue("@Id", textBox1.Text);
+            cmd.Parameters.AddWithValue("@fName", textBox2.Text);
+            cmd.Parameters.AddWithValue("@lName", textBox3.Text);
+            cmd.Parameters.AddWithValue("@Age", textBox4.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Information Successfully Updated");
+            populate();
+            Reset();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete from InfoTable where Id=@Id", con);
+            cmd.Parameters.AddWithValue("@Id", textBox1.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Information Successfully Deleted");
+            populate();
+            Reset();
+        }
+    }
+}
